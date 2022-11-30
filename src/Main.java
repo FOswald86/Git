797598374956a316f61c81fa334958c78b04c210
frank <frank.oswald@gmx.at>
2022-11-30 11:51:44 +0100

    overloaded printHelloWorld method

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,21 @@ public class Main {
     public static void main(String[] args) {
 
         printHelloWorld();
+        printHelloWorld("test");
+        printHelloWorld("another test", 10);
     }
 
     private static void printHelloWorld() {
-        for (int i = 0; i < 5; i++) {
-            System.out.println("Hello world!");
+        System.out.println("Hello world!");
+    }
+
+    private static void printHelloWorld(String output) {
+            System.out.println(output);
+    }
+
+    private static void printHelloWorld(String output, int counter) {
+        for (int i = 0; i < counter; i++) {
+            System.out.println(output);
         }
     }
 }
